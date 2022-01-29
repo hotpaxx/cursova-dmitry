073ca7b9bab027d21d9d3dd21ec0f9a144147584
Dmitry Yunyshev <capitankiller56@gmail.com>
2022-01-29 14:23:14 +0200

    first commit

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
-console.log("dima")
+const TelegramApi = require('node-telegram-bot-api')
+
+const request = require('request')
+
+const token = '5277740558:AAFOIGoOCkVg-m2YyFnHmqUwzVcNl6tlhnU'
+
+const bot = new TelegramApi(token, {polling: true})
+
+bot.onText(/\/start/, async (msg) =>{
+    console.log(msg)
+})
